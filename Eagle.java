d9caaf9ff1ab8b1773bc165ad886f98d7f69e3c8
Jean-christophe Rocher <jcrocher@orange.fr>
2019-09-30 17:24:54 +0200

    commit solution

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -12,6 +12,10 @@ public class Eagle extends Bird {
     public int getAltitude() {
         return altitude;
     }
+    
+    public void setAltitude(int meters){
+	this.altitude = meters;
+	}
 
     public boolean isFlying() {
         return flying;
@@ -21,4 +25,37 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+
+	@Override
+	public void takeOff(){
+		if (!this.flying){		
+			System.out.println(this.getName() + " takes off in the sky.");
+		}
+	}
+
+	@Override
+	public int ascends(int meters){
+		this.altitude = this.altitude + meters;
+		System.out.println(this.getName() + " fly upward, altitude : " + this.altitude);
+		return this.altitude;
+	}
+
+	@Override
+	public int descend(int meters){
+		this.altitude = this.altitude - meters;
+		System.out.println(this.getName() + " fly downward, altitude : " + this.altitude);
+		return this.altitude;	
+	}
+
+	@Override
+	public void land(){
+		if (this.altitude > 1){
+			System.out.println(this.getName() + "is too high, it can't lands.");
+		}
+		else {
+			System.out.println(this.getName() + "lands on the ground.");
+		}	
+	}
+
 }
--- Nature.java
+++ Nature.java
@@ -12,16 +12,15 @@ public class Nature {
         pingou.getOut();
 
         Eagle eyeCherry = new Eagle("Eye Cherry");
-        // TODO : décommente le code suivant afin de le tester
-        /*
+               
         eyeCherry.takeOff();
-        eyeCherry.ascend(120);
-        eyeCherry.ascend(30);
+        eyeCherry.ascends(120);
+        eyeCherry.ascends(30);
         eyeCherry.glide();
         eyeCherry.descend(140);
         eyeCherry.land();
         eyeCherry.descend(9);
         eyeCherry.land();
-        */
+        
     }
 }
